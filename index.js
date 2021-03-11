947f97595d5a6adad030ac4daa840545955aad1b
Enrique Ajin <enriquesaon@gmail.com>
2021-03-11 16:10:22 -0600

    Contact section finished. Footer section in progress.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hamburguer = document.getElementById("hamburguer")
 const menu = document.getElementById("menu")
 const close = document.getElementById("close")
+const navButtons = document.getElementsByClassName("overlay-content__item")
 
 hamburguer.onclick = function() {
     menu.style.width = "100%"
@@ -9,3 +10,10 @@ hamburguer.onclick = function() {
 close.onclick = function() {
     menu.style.width = "0"
 }
+
+
+Array.prototype.forEach.call(navButtons, button => {
+    button.onclick = () => menu.style.width = "0"
+});
+
+console.log(navButtons)
